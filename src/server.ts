16dcdeebe2d2f253ceedb0b967e6d9909e3924d2
Aline Avelino <aline.savelino@gmail.com>
2022-07-04 22:27:30 -0300

    :sparkles: feat: create the server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Teste dois");
+import express from "express";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
