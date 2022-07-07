cf1d9ffa37e44337806f91202e2f279a0aeddbbe
Aline Avelino <aline.savelino@gmail.com>
2022-07-06 22:03:10 -0300

    ::sparkles: feat: create get listAllTransactions

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
+import { prismaClient } from "../database/PrismaClient";
 import { TransactionService } from "../services/TransactionService";
 
 class TransactionController {
   async create(req: Request, res: Response) {
     const service = new TransactionService();
-    console.log(req.body);
+
     try {
       const { title, value, type, category, date } = req.body;
       const transaction = await service.execute(
@@ -17,6 +18,16 @@ class TransactionController {
       return res.json(transaction);
     } catch (error) {
       res.status(400).json(error);
+    }
+  }
+
+  async listAllTransactions(req: Request, res: Response) {
+    try {
+      const transactions = await prismaClient.transaction.findMany();
+
+      return res.status(200).json(transactions);
+    } catch (error) {
+      return res.status(400).json(error);
     }
   }
 }
--- src/routes/TransactionRoutes.ts
+++ src/routes/TransactionRoutes.ts
@@ -5,5 +5,9 @@ const express = require("express");
 const transactionRoutes = express.Router();
 
 transactionRoutes.post("/transactions", transactionController.create);
+transactionRoutes.get(
+  "/transactions",
+  transactionController.listAllTransactions
+);
 
 export default transactionRoutes;
