5b7ef5e32a6f943871269baf3a6ad26d1fc78bf5
Henrique Leme de Brito <henriqueleme.brito@outlook.com>
2022-07-23 19:34:27 -0300

    📚 docs: add swagger
    Add swagger to start of documentation

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import routes from "./routes/routes";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 const port = 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(routes);
 
 app.listen(process.env.PORT || port, () => {
